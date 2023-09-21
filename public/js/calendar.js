1c9ad8d57934e09f33274b57e00d7c6935a46ae3
StevenLu8050 <silvercrow7777@gmail.com>
2023-09-21 22:02:00 +0800

    針對calendar html css js，去重新編寫新增功能。(未完成)

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,46 +1,81 @@
-$('#calendar').fullCalendar({
-    editable: true,
-    events: [{
-      title: '昨天的活動',
-      start: moment().subtract(1, 'days').format('YYYY-MM-DD')
-    }, {
-      title: '持續一周的活動',
-      start: moment().add(7, 'days').format('YYYY-MM-DD'),
-      end: moment().add(14, 'days').format('YYYY-MM-DD'),
-      color: 'lightBlue'
-    }],
-    dayClick: function(date, event, view) {
-      console.log('add event');
-      console.log(date);
-      console.log(event);
-      console.log(view);
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var today = new Date();
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    initialDate: today.toISOString().split('T')[0],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
     },
-    eventClick: function(date, event, view) {
-      console.log('modify event');
-      console.log(date);
-      console.log(event);
-      console.log(view);
-    }
+    editable: true,  
+    dayMaxEvents: true,
+    events: [
+      {
+        title: 'All Day Event',
+        start: '2023-09-01'
+      },
+      {
+        title: 'Long Event',
+        start: '2023-09-07',
+        end: '2023-09-10'
+      },
+      {
+        groupId: '999',
+        title: 'Repeating Event',
+        start: '2023-09-09T16:00:00'
+      },
+      {
+        groupId: '999',
+        title: 'Repeating Event',
+        start: '2023-09-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2023-09-11',
+        end: '2023-09-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2023-09-12T10:30:00',
+        end: '2023-09-12T12:30:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2023-09-12T10:30:00',
+        end: '2023-09-12T12:30:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2023-09-12T10:30:00',
+        end: '2023-09-12T12:30:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2023-09-12T10:30:00',
+        end: '2023-09-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2023-09-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2023-09-12T14:30:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2023-09-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'https://google.com/',
+        start: '2023-09-28'
+      }
+    ]
   });
-  
-  $('#calendar').fullCalendar('renderEvent', {
-    title: '明天的活動',
-    start: moment().add(1, 'days').format('YYYY-MM-DD')
-  });
-  
-  $('#calendar').fullCalendar('renderEvent', {
-    id: 'eventGroup1',
-    title: '活動1',
-    start: moment().add(3, 'days').format('YYYY-MM-DD'),
-    textColor: 'black',
-    color: 'beige'
-  });
-  
-  $('#calendar').fullCalendar('renderEvent', {
-    id: 'eventGroup1',
-    title: '活動2',
-    start: moment().add(5, 'days').format('YYYY-MM-DD'),
-    textColor: 'black',
-    color: 'beige'
-  });
-  
+
+  calendar.render();
+});
