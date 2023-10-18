460db86d53ea8785c61402cb13d8271643974283
s10955031 <vicky910411@gmail..com>
2023-10-19 05:29:34 +0800

    增加教師端主題探討

--- public/js/teacher_research.js
+++ public/js/teacher_research.js
@@ -3,10 +3,12 @@ const account = sessionStorage.getItem('account');
 // 获取打开和关闭 modal dialog 的元素
 var openModalButton = document.getElementById("addCourse");
 var modal = document.getElementById("myModal");
+var modalcontent = document.getElementById("addmodal");
 
 // 点击 "新增课程" 按钮时打开 modal dialog
 openModalButton.onclick = function() {
     modal.style.display = "block";
+    modalcontent.style.left = "50%";
 }
 
 
@@ -111,7 +113,41 @@ document.addEventListener('DOMContentLoaded', async function() {
         // 获取course-container元素，并将新的course-box添加到其中
         var courseContainer = document.querySelector(".course-container");
         courseContainer.appendChild(newCourseBox);
+    });
 
+    // 添加点击事件监听器到每个 course-box 元素
+    document.querySelectorAll(".course-box").forEach(function(courseBox) {
+        courseBox.addEventListener("click", async function() {
+            // 获取相应的数据，例如根据 courseBox 内的 subject 获取数据
+            const subject = courseBox.querySelector("h2").textContent; // 从 h2 元素中获取 subject
+            const topic = courseBox.querySelector("p").textContent; // 从 h2 元素中获取 subject
+            const course =  await DB_API.getQuestion(topic);
+            const coursemember = await DB_API.getCoursemember(topic);
+            // 在这里，你可以根据 subject 从数据库或其他数据源中获取相关内容
+            // 假设获取到了 title 和 content 数据
+            const title = "标题示例：" + subject;
+            const content = "内容示例：" + topic;
+            const text = "問題一：" + course.question1;
+            var ml = document.getElementById("memberlist");
+            coursemember.forEach((coursememberData, index) => {
+                // 這裡假設每個任務都是一個對象，您可以根據您的數據結構進行調整
+                ml.innerHTML = `<ol><li>${index + 1} . ${coursememberData.name}</li></ol>`;
+            });
+     
+            // 填充模态对话框的内容
+            document.getElementById("modalTitle").textContent = title;
+            document.getElementById("modalContent").textContent = content;
+            document.getElementById("modalQuestion1").textContent = text;
+            // 打开模态对话框
+            const modal = document.getElementById("courseModal");
+            modal.style.display = "flex";
+            });
+    });
+
+    // 关闭模态对话框的按钮
+    document.querySelector(".close").addEventListener("click", function() {
+    const modal = document.getElementById("courseModal");
+    modal.style.display = "none";
     });
 });
 
